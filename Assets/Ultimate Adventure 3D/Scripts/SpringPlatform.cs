d731bee7f4c623f42e919573476cc81f005dfa80
Mariya Kolodizhanskaya <fishillness@gmail.com>
2022-11-19 23:15:13 +0700

    Srcipt "SpringPlatform"
    Added srcipt "SpringPlatform"

---
+++ Assets/Ultimate Adventure 3D/Scripts/SpringPlatform.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using SimpleFPS;
+
+public class SpringPlatform : MonoBehaviour
+{
+    [SerializeField] private int jumpForce;
+    private float previusJumpForce;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        FirstPersonController fps = other.GetComponent<FirstPersonController>();
+
+        if (fps != null)
+        {
+            previusJumpForce = fps.m_JumpSpeed;
+            fps.m_JumpSpeed += jumpForce;
+            fps.m_Jump = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        FirstPersonController fps = other.GetComponent<FirstPersonController>();
+
+        if (fps != null)
+        {
+            fps.m_JumpSpeed = previusJumpForce;
+        }
+    }
+}
