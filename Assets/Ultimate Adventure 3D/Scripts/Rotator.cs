01f7884541ba06c332db3c2af44988957e7fe277
Mariya Kolodizhanskaya <fishillness@gmail.com>
2022-11-21 17:24:59 +0700

    Scripts "MoveTo" and "Rotator"
    Added scripts "MoveTo" and "Rotator"

---
+++ Assets/Ultimate Adventure 3D/Scripts/Rotator.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField] private Vector3 speed;
+    [SerializeField] private Transform targetTransform;
+
+    private void Update()
+    {
+        targetTransform.Rotate(speed * Time.deltaTime);
+    }
+}
